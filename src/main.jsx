3533b6b84c53c5b85d6db71eeeb41ea9fe66f480
Lê Phan Hải Long <longlph.23it@gmail.com>
2025-02-26 09:15:16 +0700

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router";
 import './index.css'
 import App from './App.jsx'
-
+import Home from './pages/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+]);
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} >
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </RouterProvider>
+
 )
---
+++ src/pages/ListProject.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListProject = () => {
+  return (
+    <div>
+      ListProject
+    </div>
+  )
+}
+
+export default ListProject
